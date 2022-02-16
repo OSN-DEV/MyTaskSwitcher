28428c094f77d6ce140ddf9acd272e8c52bb1fc6
OSN-DEV <osndev1973@gmail.com>
2022-02-16 23:22:02 +0900

    Icon表示の追加

--- UI/TaskGrid/TaskGridViewModel.cs
+++ UI/TaskGrid/TaskGridViewModel.cs
@@ -1,5 +1,4 @@
-﻿using MyTaskSwitcher.AppCommon;
-using MyTaskSwitcher.Data;
+﻿using MyTaskSwitcher.Data;
 using MyTaskSwitcher.Func;
 using OsnCsLib.WPFComponent.Bind;
 using System;
@@ -7,12 +6,11 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Windows.Media.Imaging;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Windows.Media.Imaging;
 
 namespace MyTaskSwitcher.UI.TaskGrid {
     public class TaskGridViewModel : BaseBindable {
@@ -20,19 +18,19 @@ namespace MyTaskSwitcher.UI.TaskGrid {
         #region Win32 API
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool EnumWindows(EnumWindowsDelegate lpEnumFunc, IntPtr lparam);
+        private static extern bool EnumWindows(EnumWindowsDelegate lpEnumFunc, IntPtr lparam);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern int GetWindowTextLength(IntPtr hWnd);
+        private static extern int GetWindowTextLength(IntPtr hWnd);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+        private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
 
         [DllImport("user32.dll")]
-        public static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+        private static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
 
         //[DllImport("user32.dll")]
         //public static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
@@ -43,18 +41,21 @@ namespace MyTaskSwitcher.UI.TaskGrid {
         private static extern IntPtr GetWindowLongPtr64(IntPtr hWnd, int nIndex);
 
         [DllImport("dwmapi.dll")]
-        public static extern long DwmGetWindowAttribute(IntPtr hWnd, DWMWINDOWATTRIBUTE dwAttribute, out IntPtr pvAttribute, int cbAttribute);
+        private static extern long DwmGetWindowAttribute(IntPtr hWnd, DWMWINDOWATTRIBUTE dwAttribute, out IntPtr pvAttribute, int cbAttribute);
 
         [DllImport("user32.dll")]
-        public static extern IntPtr GetWindow(IntPtr hWnd, int wCmd);
+        private static extern IntPtr GetWindow(IntPtr hWnd, int wCmd);
 
         [DllImport("User32.Dll")]
-        public static extern IntPtr GetParent(IntPtr hWnd);
+        private static extern IntPtr GetParent(IntPtr hWnd);
 
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern int GetWindowInfo(IntPtr hwnd, ref WINDOWINFO pwi);
+        private static extern int GetWindowInfo(IntPtr hwnd, ref WINDOWINFO pwi);
 
-        public delegate bool EnumWindowsDelegate(IntPtr hWnd, IntPtr lparam);
+        [DllImport("user32.dll")]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        private delegate bool EnumWindowsDelegate(IntPtr hWnd, IntPtr lparam);
 
 
         [StructLayout(LayoutKind.Sequential)]
@@ -265,47 +266,15 @@ namespace MyTaskSwitcher.UI.TaskGrid {
 
             EnumWindows(new EnumWindowsDelegate(EnumWindowCallBack), IntPtr.Zero);
 
-            //// test
-            //var excludeList = new List<string> { "SystemSettings", "TextInputHost", "ApplicationFrameHost" };
-
-            //Process[] processes = Process.GetProcesses();
-            //foreach (var p in processes) {
-            //    if (0 < p.MainWindowTitle.Length) {
-            //        if (p.MainWindowTitle.StartsWith("MyTaskSwither")) {
-            //            continue;
-            //        }
-
-            //        Debug.WriteLine($"{p.ProcessName}:{p.MainWindowTitle}");
-            //        if (-1 != excludeList.IndexOf(p.ProcessName)) {
-            //            continue;
-            //        }
-
-            //        var iconFile = $"{Constant.IconCache}\\{p.ProcessName}";
-            //        if (!File.Exists(iconFile)) {
-            //            var icon = GetAppIcon(p.MainWindowHandle);
-
-            //            if (File.Exists(iconFile)) {
-            //                File.Delete(iconFile);
-            //            }
-            //            if (null == icon) {
-            //                continue;
-            //            }
-            //            icon.ToBitmap().Save(iconFile, ImageFormat.Png);
-            //        }
-            //        var item = new TaskItem(p.MainWindowHandle, p.MainWindowTitle);
-            //        item.Icon = this.GetBitmapSource(iconFile);
-            //        this._itemList.Add(item);
-            //    }
-            //}
-
-            //var rest = this._itemList.Count % ItemCountPerPage;
-            //if (0 < rest) {
-            //    rest = ItemCountPerPage - rest;
-            //}
-            //for (int i = 0; i < rest; i++) {
-            //    this._itemList.Add(new TaskItem());
-            //}
-            //this._maxIndex = this._itemList.Count / ItemCountPerPage - 1;
+            var rest = this._itemList.Count % ItemCountPerPage;
+            if (0 < rest) {
+                rest = ItemCountPerPage - rest;
+            }
+            for (int i = 0; i < rest; i++) {
+                this._itemList.Add(new TaskItem());
+            }
+            this._maxIndex = this._itemList.Count / ItemCountPerPage - 1;
+            this.ShowPage(0);
         }
 
 
@@ -319,43 +288,49 @@ namespace MyTaskSwitcher.UI.TaskGrid {
             bool hasOwner = ((IntPtr)0 != GetWindow(hWnd, GW_OWNER));
             bool isChild = ((IntPtr)0 != GetParent(hWnd));
             if (IsWindowAvailable(hWnd) && !hasOwner && !isChild) {
-                StringBuilder tsb = new StringBuilder(textLen + 1);
-                GetWindowText(hWnd, tsb, tsb.Capacity);
-                Console.WriteLine("タイトル:" + tsb.ToString());
 
-                var wi = new WINDOWINFO();
-                if (0 != GetWindowInfo(hWnd, ref wi)) {
-                    StringBuilder csb = new StringBuilder(256);
-                    GetClassName(hWnd, csb, csb.Capacity);
+                //StringBuilder tsb = new StringBuilder(textLen + 1);
+                //GetWindowText(hWnd, tsb, tsb.Capacity);
+                //Console.WriteLine("タイトル:" + tsb.ToString());
 
-                    // Console.WriteLine("クラス名:" + csb.ToString());
-                    //int textLen = GetWindowTextLength(hWnd);
-                    //if (0 < textLen) {
-                    //    StringBuilder tsb = new StringBuilder(textLen + 1);
-                    //    GetWindowText(hWnd, tsb, tsb.Capacity);
-                    //    Console.WriteLine("タイトル:" + tsb.ToString());
-                    //} else {
-                    //    //                        Console.WriteLine("No タイトル:");
-                    //}
-                }
-            }
+                //var wi = new WINDOWINFO();
+                //if (0 != GetWindowInfo(hWnd, ref wi)) {
+                //    StringBuilder csb = new StringBuilder(256);
+                //    GetClassName(hWnd, csb, csb.Capacity);
+
+                //    Console.WriteLine("クラス名:" + csb.ToString());
+                //    int textLen = GetWindowTextLength(hWnd);
+                //    if (0 < textLen) {
+                //        StringBuilder tsb = new StringBuilder(textLen + 1);
+                //        GetWindowText(hWnd, tsb, tsb.Capacity);
+                //        Console.WriteLine("タイトル:" + tsb.ToString());
+                //    } else {
+                //        //                        Console.WriteLine("No タイトル:");
+                //    }
+                //}
+
+                int processID;
+                Icon icon;
+                Process process;
+
+                // ウィンドウハンドル→プロセスID
+                GetWindowThreadProcessId(hWnd, out processID);
+
+                // プロセスID→プロセス
+                process = Process.GetProcessById(processID);
+
+                // プロセス→実行ファイル名→アイコン
+                icon = System.Drawing.Icon.ExtractAssociatedIcon(process.MainModule.FileName);
             
 
-            ////ウィンドウのタイトルの長さを取得する
-            //int textLen = GetWindowTextLength(hWnd);
-            //if (0 < textLen) {
-            //    //ウィンドウのタイトルを取得する
-            //    StringBuilder tsb = new StringBuilder(textLen + 1);
-            //    GetWindowText(hWnd, tsb, tsb.Capacity);
 
-            //    //ウィンドウのクラス名を取得する
-            //    StringBuilder csb = new StringBuilder(256);
-            //    GetClassName(hWnd, csb, csb.Capacity);
+            StringBuilder tsb = new StringBuilder(textLen + 1);
+                GetWindowText(hWnd, tsb, tsb.Capacity);
+                var item = new TaskItem(hWnd, tsb.ToString());
+                item.Icon = ConverToSource(icon.ToBitmap());
+                this._itemList.Add(item);
+            }
 
-            //    //結果を表示する
-            //    Console.WriteLine("クラス名:" + csb.ToString());
-            //    Console.WriteLine("タイトル:" + tsb.ToString());
-            //}
 
             //すべてのウィンドウを列挙する
             return true;
@@ -366,24 +341,16 @@ namespace MyTaskSwitcher.UI.TaskGrid {
             RECT rc = new RECT();
             GetWindowRect(hWnd, out rc);
 
-            // bool isVisible = ((long)GetWindowLongPtr(hWnd, GWL_STYLE) & WS_VISIBLE) == WS_VISIBLE; // 可視状態
             bool isVisible = ((long)GetWindowLongPtr(hWnd, GWL_STYLE) & WS_VISIBLE) != 0; // 可視状態
-            // bool isToolWindow = ((int)GetWindowLongPtr(hWnd, GWL_EXSTYLE) & WS_EX_TOOLWINDOW) == WS_EX_TOOLWINDOW; // ツールウインドウ
             bool isToolWindow = ((int)GetWindowLongPtr(hWnd, GWL_EXSTYLE) & WS_EX_TOOLWINDOW) != 0; // ツールウインドウ
 
             if (isVisible && !isToolWindow && (0 != rc.right- rc.left) && (0 != rc.bottom - rc.top)) {
-            //    Console.WriteLine(GetWindowLongPtr(hWnd, GWL_EXSTYLE) & WS_EX_NOREDIRECTIONBITMAP);
-
-                // bool isRendered = ((int)GetWindowLongPtr(hWnd, GWL_EXSTYLE) & WS_EX_NOREDIRECTIONBITMAP) == WS_EX_NOREDIRECTIONBITMAP;
                 bool isRendered = ((int)GetWindowLongPtr(hWnd, GWL_EXSTYLE) & WS_EX_NOREDIRECTIONBITMAP) != 0;
-//                 Console.WriteLine("isRendered:" + isRendered);
                 if (isRendered) {
                     var result = DwmGetWindowAttribute(hWnd, DWMWINDOWATTRIBUTE.DWMWA_CLOAKED, out IntPtr isCloaked, Marshal.SizeOf(typeof(bool)));
                     if (result != 0) {
-                        Console.WriteLine("result:" + result);
                         return false;
                     }
-                    // Console.WriteLine("isCloaked:" + isCloaked);
                     if ((IntPtr)0 != isCloaked) {
                         return false;
                     }
@@ -395,6 +362,61 @@ namespace MyTaskSwitcher.UI.TaskGrid {
         }
 
 
+
+
+
+        ////あの手この手でアイコンを取得[小さい]
+        //HICON GetSmallIconFromWindow(HWND hWnd) {
+        //    HICON hIcon = NULL;
+
+        //    if (hWnd == NULL || IsHungAppWindow(hWnd)) return NULL;
+
+        //    if (!SendMessageTimeout(hWnd, WM_GETICON, ICON_SMALL, 0, SMTO_ABORTIFHUNG, 500, (PDWORD_PTR) & hIcon)) {
+        //        DWORD dwError = GetLastError();
+
+        //        if (dwError == ERROR_SUCCESS || dwError == ERROR_TIMEOUT) {
+        //            return LoadIcon(NULL, IDI_APPLICATION);
+        //        }
+        //    }
+        //    if (hIcon == NULL) {
+        //        hIcon = (HICON)GetClassLongPtr(hWnd, GCLP_HICONSM);
+        //    }
+        //    if (hIcon == NULL) {
+        //        HWND hParent = GetParent(hWnd);
+        //        if (IsWindowVisible(hParent)) return GetSmallIconFromWindow(hParent);
+        //    }
+        //    return hIcon;
+        //}
+
+        ////あの手この手でアイコンを取得[大きい]
+        //HICON GetLargeIconFromWindow(HWND hWnd) {
+        //    HICON hIcon = NULL;
+
+        //    if (hWnd == NULL || IsHungAppWindow(hWnd)) return NULL;
+
+        //    if (!SendMessageTimeout(hWnd, WM_GETICON, ICON_BIG, 0, SMTO_ABORTIFHUNG, 500, (PDWORD_PTR) & hIcon)) {
+        //        DWORD dwError = GetLastError();
+
+        //        if (dwError == ERROR_SUCCESS || dwError == ERROR_TIMEOUT) {
+        //            return LoadIcon(NULL, IDI_APPLICATION);
+        //        }
+        //    }
+        //    if (hIcon == NULL) {
+        //        hIcon = (HICON)GetClassLongPtr(hWnd, GCLP_HICON);
+        //    }
+        //    if (hIcon == NULL) {
+        //        HWND hParent = GetParent(hWnd);
+        //        if (IsWindowVisible(hParent)) return GetLargeIconFromWindow(hParent);
+        //    }
+        //    return hIcon;
+        //}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="nIndex"></param>
+        /// <returns></returns>
         private long GetWindowLongPtr(IntPtr hWnd, int nIndex) {
             if (IntPtr.Size == 8)
                 return GetWindowLongPtr64(hWnd, nIndex).ToInt64();
@@ -408,7 +430,6 @@ namespace MyTaskSwitcher.UI.TaskGrid {
         /// <param name="index"></param>
         private void ShowPage(int index) {
             // this.ItemList.Clear();
-            return;
 
             var offset = index * ItemCountPerPage;
             for (int i = 0; i < ItemCountPerPage; i++) {
